073c48f08422aa0ba0ea69953a0f96e97bfb5087
OjasviSK <80693311+OjasviSK@users.noreply.github.com>
2021-06-15 22:54:41 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# makeIt_fall1
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sprites.js
@@ -0,0 +1,10 @@
+class Boxes extends Baseclass{
+    constructor(x, y){
+        super(x, y, 25, 40);
+    }
+    display(){
+        fill("purple");
+        super.display();
+    }
+
+}
